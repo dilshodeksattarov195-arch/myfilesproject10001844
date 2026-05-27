35fed6076c7a8dac01acef2eb4834413437b0dda
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 07:11:39 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cacheCalidateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cacheCalidateWorker()
+    print("Worker engine initialized.")
